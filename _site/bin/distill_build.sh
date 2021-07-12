3099f25e6390cad8f7f55869c241bf8a2d1911ec
sykdomspulsen <sykdomspulsen@fhi.no>
2021-07-12 12:37:40 +0200

    [ci-skip]

--- _site/bin/distill_build.sh
+++ _site/bin/distill_build.sh
@@ -2,9 +2,9 @@
 
 echo Running $1 in testbash
 echo " "
-sudo podman run --rm \
+sudo podman run --privileged --rm \
   -v $WORKSPACE:/git/sykdomspulsen-dokumentasjon \
-  sykdomspulsen:bash Rscript -e 'rmarkdown::render_site("/git/sykdomspulsen-dokumentasjon")'
+  dsyk:bash_interactive Rscript -e 'rmarkdown::render_site("/git/sykdomspulsen-dokumentasjon")'
 
 #sudo mv -r $WORKSPACE/sykdomspulsen/docs/reference $WORKSPACE/_site/
 sudo chmod -R 777 $WORKSPACE
