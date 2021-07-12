b3acba6212a5169f091c997521d97adcc840b023
Richard Aubrey White <RichardAubrey.White@fhi.no>
2021-07-12 12:35:22 +0200

    Fixing build command

--- bin/distill_build.sh
+++ bin/distill_build.sh
@@ -4,7 +4,7 @@ echo Running $1 in testbash
 echo " "
 sudo podman run --rm \
   -v $WORKSPACE:/git/sykdomspulsen-dokumentasjon \
-  sykdomspulsen:bash Rscript -e 'rmarkdown::render_site("/git/sykdomspulsen-dokumentasjon")'
+  dsyk:bash_interactive Rscript -e 'rmarkdown::render_site("/git/sykdomspulsen-dokumentasjon")'
 
 #sudo mv -r $WORKSPACE/sykdomspulsen/docs/reference $WORKSPACE/_site/
 sudo chmod -R 777 $WORKSPACE
