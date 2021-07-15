20fb73f54710bf88a49eea8d185d84b95d36e9f6
Richard Aubrey White <RichardAubrey.White@fhi.no>
2021-07-15 11:26:36 +0200

    tutorial_1_introduction

--- bin/distill_build.sh
+++ bin/distill_build.sh
@@ -4,7 +4,7 @@ echo Running $1 in testbash
 echo " "
 sudo podman run --privileged --rm \
   -v $WORKSPACE:/git/sykdomspulsen-dokumentasjon \
-  dsyk:bash_interactive Rscript -e 'rmarkdown::render_site("/git/sykdomspulsen-dokumentasjon")'
+  dsyk:bash_interactive Rscript -e 'rmarkdown::render_site("/git/sykdomspulsen-dokumentasjon", envir = new.env())'
 
 #sudo mv -r $WORKSPACE/sykdomspulsen/docs/reference $WORKSPACE/_site/
 sudo chmod -R 777 $WORKSPACE
